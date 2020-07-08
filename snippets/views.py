abb57f3212f782c589f98cb505848349962d586c
tuan anh <canhazn@gmail.com>
2020-07-08 15:00:08 +0700

    Hyprtlink relationships

--- snippets/views.py
+++ snippets/views.py
@@ -4,9 +4,29 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-class SnippetsList(generics.ListCreateAPIView):
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *arg, **karg):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
@@ -26,6 +46,6 @@ class UserList(generics.ListAPIView):
     serializer_class = UserSerializer
 
 
-class USerDetail(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
